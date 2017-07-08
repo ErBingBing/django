79ba7e117a043f0e0790e92881314973c5cf08dc
erbingbing <913702626@qq.com>
2017-07-08 16:18:10 +0800

    首页

--- shopping/ttsx_user/views.py
+++ shopping/ttsx_user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
+
 
 # Create your views here.
+def index(request):
+    # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # return HttpResponse(BASE_DIR)
+    return render(request, 'ttsx_user/index.html')
